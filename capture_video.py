4b460ca826b87b69cece4e6bf466825326169ca6
SuhasBRao <raos04567@gmail.com>
2021-05-11 13:44:24 +0530

    Added new file

--- capture_video.py
+++ capture_video.py
@@ -3,9 +3,6 @@ import numpy as np
 
 cam = cv2.VideoCapture(0)
 num_frames = 0
-fgbg = cv2.createBackgroundSubtractorMOG2()
-global background
-
 
 ROI_top = 100
 ROI_bottom = 400
@@ -25,11 +22,6 @@ while True:
     
     roi = frame[ROI_top:ROI_bottom, ROI_right:ROI_left]
     
-    gray_frame = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-    gray_frame = cv2.GaussianBlur(gray_frame, (15, 15), 0)
-
-
-    
     cv2.putText(frame_copy, "Now you can gesture", (80, 400), 
     cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,255,0), 2)
                    
@@ -38,13 +30,6 @@ while True:
     # incrementing the number of frames for tracking
     num_frames += 1
 
-    #cv2.imshow('fg',fgmask)
-
-    # Display the frame with segmented hand
-    
-    # Get the area of the image as a comparison
-
-    
     cv2.rectangle(frame_copy, (ROI_left, ROI_top), (ROI_right,ROI_bottom), (255,128,0), 3)
     
     cv2.putText(frame_copy, "Sign language recognition_ _ _",
